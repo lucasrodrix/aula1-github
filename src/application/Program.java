22630ad2acac6c76d0d6ab752e7083a8f98f5fe2
Lucas Rodrix <lucas@gruporodrix.net>
2019-09-17 11:17:54 -0300

    Inserido mensagem de Boa Noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,6 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Bom Dia!");
 		System.out.println("Boa Tarde!");
+		System.out.println("Boa Noite!");
 	}
-
 }
